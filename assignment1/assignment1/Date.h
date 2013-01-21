bb183dceac4c3f8050e7167fda45ebf3aed0b63d
nut juangmarisakul <dentistnut@gmail.com>
2013-01-21 18:38:03 +0700

    add the assignment file

---
+++ assignment1/assignment1/Date.h
@@ -0,0 +1,26 @@
+#ifndef _Date_h
+#define _Date_h
+
+#include<string>
+
+class Date(){
+	const int day = 1;
+	const int years = 2000;
+	const string month = "January";
+	Date();
+	Date(int, int, string);
+	
+	const int getDay() const;
+	const int getYear() const;
+	const string getMonth() const;
+
+	string toString();
+	int daysInMonth(string, int);
+	bool isLeapYear(int);
+	enum Month{};
+	string monthToString(Month);
+
+};
+
+
+#endif
